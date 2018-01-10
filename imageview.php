bede8feab70aa1cdc9bc4452489ee73043e3f296
Jack McKernan <jmcker@outlook.com>
2018-01-10 16:23:15 -0500

    Format whitespace

--- imageview.php
+++ imageview.php
@@ -38,14 +38,14 @@
 
 <!DOCTYPE HTML>
 <html>
-	<head>
-		<title><?php echo $photoname?> - Yoga Balance</title>
-		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1" />
-		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
-		<link rel="stylesheet" href="assets/css/main.css" />
-		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
-		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+    <head>
+        <title><?php echo $photoname?> - Yoga Balance</title>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+        <link rel="stylesheet" href="assets/css/main.css" />
+        <!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+        <!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
         <link rel="stylesheet" href="assets/css/mailchimp.css" type="text/css" />
         <link rel="apple-touch-icon" sizes="57x57" href="images/favicon/apple-icon-57x57.png">
         <link rel="apple-touch-icon" sizes="60x60" href="images/favicon/apple-icon-60x60.png">
@@ -64,45 +64,45 @@
         <meta name="msapplication-TileColor" content="#663366">
         <meta name="msapplication-TileImage" content="images/favicon/ms-icon-144x144.png">
         <meta name="theme-color" content="#663366">
-	</head>
-	<body>
+    </head>
+    <body>
 
-		<!-- Page Wrapper -->
-			<div id="page-wrapper">
+        <!-- Page Wrapper -->
+            <div id="page-wrapper">
 
-				<!-- Header -->
-					<header id="header">
-						<h1><a href="index.html">Yoga Balance</a></h1>
-						<nav>
+                <!-- Header -->
+                    <header id="header">
+                        <h1><a href="index.html">Yoga Balance</a></h1>
+                        <nav>
                             <a href="http://www.yelp.com/biz/yoga-balance-grayslake" class="social" onclick="trackOutboundLink('Yelp - Header');" target="_blank">
                                 <i class="fa fa-yelp fa-lg"></i>
                             </a>
                             <a href="https://www.facebook.com/Yoga-Balance-Inc-949307748415341/" class="social" onclick="trackOutboundLink('Facebook - Header');" target="_blank">
                                 <i class="fa fa-facebook fa-lg"></i></a>&nbsp;&nbsp;
-							<a href="#menu">Menu</a>
-						</nav>
-					</header>
+                            <a href="#menu">Menu</a>
+                        </nav>
+                    </header>
 
-				<!-- Menu -->
-					<nav id="menu">
-						<div class="inner">
-							<h2>Menu</h2>
-							<ul class="links">
-								<li><a href="index.html">Home</a></li>
-								<li><a href="index.html#about">About</a></li>
+                <!-- Menu -->
+                    <nav id="menu">
+                        <div class="inner">
+                            <h2>Menu</h2>
+                            <ul class="links">
+                                <li><a href="index.html">Home</a></li>
+                                <li><a href="index.html#about">About</a></li>
                                 <li><a href="index.html#classes">Classes</a></li>
                                 <!---<li><a href="faq.html">New to Yoga?</a></li>--->
-								<li><a href="index.html#pictures">Pictures</a></li>
-								<li><a href="index.html#contact">Contact</a></li>
-							</ul>
-							<a href="#" class="close">Close</a>
-						</div>
-					</nav>
+                                <li><a href="index.html#pictures">Pictures</a></li>
+                                <li><a href="index.html#contact">Contact</a></li>
+                            </ul>
+                            <a href="#" class="close">Close</a>
+                        </div>
+                    </nav>
 
 
-						<!-- Content -->
-							<div class="wrapper">
-								<div class="inner">
+                        <!-- Content -->
+                            <div class="wrapper">
+                                <div class="inner">
                                     <div style="margin-bottom: 3em; margin-top: -1em; text-align: center;">
                                         <?php 
                                             if (empty($error))
@@ -116,39 +116,39 @@
                                             }
                                         ?>
                                     </div>
-								</div>
-							</div>
+                                </div>
+                            </div>
 
-					</section>
+                    </section>
 
-				<a name="contact" class="anchor"></a>
-				<!-- Footer -->
-					<section id="footer">
-						<div class="inner">
-							<h2 class="major">Get in touch</h2>
-							<p></p>
-							
+                <a name="contact" class="anchor"></a>
+                <!-- Footer -->
+                    <section id="footer">
+                        <div class="inner">
+                            <h2 class="major">Get in touch</h2>
+                            <p></p>
+                            
                             <!-- Begin MailChimp Signup Form -->
                             <div class="mailchimp" id="mc_embed_signup">
                                 <form action="//facebook.us11.list-manage.com/subscribe/post?u=7d18fc5a03ab56d88426b1f19&amp;id=a3d60e38d9" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" onsubmit="trackFormSubmission('Mailchimp Subscription - Image View');" target="_blank" novalidate>
                                     <div id="mc_embed_signup_scroll">
-	                                    <h2>Subscribe to our mailing list!</h2>
+                                        <h2>Subscribe to our mailing list!</h2>
                                         <div class="mc-field-group">
-	                                        <label for="mce-FNAME">First Name </label>
-	                                        <input type="text" value="" name="FNAME" class="required" id="mce-FNAME">
+                                            <label for="mce-FNAME">First Name </label>
+                                            <input type="text" value="" name="FNAME" class="required" id="mce-FNAME">
                                         </div>
                                         <div class="mc-field-group">
-	                                        <label for="mce-LNAME">Last Name </label>
-	                                        <input type="text" value="" name="LNAME" class="required" id="mce-LNAME">
+                                            <label for="mce-LNAME">Last Name </label>
+                                            <input type="text" value="" name="LNAME" class="required" id="mce-LNAME">
                                         </div>
                                         <div class="mc-field-group">
-	                                        <label for="mce-EMAIL">Email Address </label>
-	                                        <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+                                            <label for="mce-EMAIL">Email Address </label>
+                                            <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
                                         </div>
-	                                    <div id="mce-responses" class="clear">
-		                                    <div class="response" id="mce-error-response" style="display:none"></div>
-		                                    <div class="response" id="mce-success-response" style="display:none"></div>
-	                                    </div>
+                                        <div id="mce-responses" class="clear">
+                                            <div class="response" id="mce-error-response" style="display:none"></div>
+                                            <div class="response" id="mce-success-response" style="display:none"></div>
+                                        </div>
                                         <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_7d18fc5a03ab56d88426b1f19_a3d60e38d9" tabindex="-1" value=""></div>
                                         <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
                                     </div><br />
@@ -157,32 +157,32 @@
                             </div>
                             <!--End MailChimp Signup Form-->
 
-							<ul class="contact">
-								<li class="fa-home">
-									Yoga Balance Inc<br />
-									997 N Corporate Circle, Suite B<br />
-									Grayslake, IL 60030
-								</li>
-								<li class="fa-phone">(847) 807-1508</li>
-								<li class="fa-envelope"><a href="mailto:lisa.yogabalance@gmail.com" target="_blank">lisa.yogabalance@gmail.com</a></li>
-								<li class="fa-facebook"><a href="https://www.facebook.com/Yoga-Balance-Inc-949307748415341/" onclick="trackOutboundLink('Facebook - Footer');" target="_blank">facebook.com/Yoga-Balance-Inc</a></li>
+                            <ul class="contact">
+                                <li class="fa-home">
+                                    Yoga Balance Inc<br />
+                                    997 N Corporate Circle, Suite B<br />
+                                    Grayslake, IL 60030
+                                </li>
+                                <li class="fa-phone">(847) 807-1508</li>
+                                <li class="fa-envelope"><a href="mailto:lisa.yogabalance@gmail.com" target="_blank">lisa.yogabalance@gmail.com</a></li>
+                                <li class="fa-facebook"><a href="https://www.facebook.com/Yoga-Balance-Inc-949307748415341/" onclick="trackOutboundLink('Facebook - Footer');" target="_blank">facebook.com/Yoga-Balance-Inc</a></li>
                                 <li class="fa-yelp"><a href="http://www.yelp.com/biz/yoga-balance-grayslake" onclick="trackOutboundLink('Yelp - Footer');" target="_blank">yelp.com/biz/yoga-balance-grayslake</a></li>
-							</ul>
-							<ul class="copyright">
-								<li>&copy; Yoga Balance Inc. All rights reserved.</li><li>Design: <a href="mailto:webmaster@yogabalanceinc.com" target="_blank">Jack McKernan</a></li><li>Template: <a href="http://html5up.net" target="_blank">HTML5 UP</a></li>
-							</ul>
-						</div>
-					</section>
+                            </ul>
+                            <ul class="copyright">
+                                <li>&copy; Yoga Balance Inc. All rights reserved.</li><li>Design: <a href="mailto:webmaster@yogabalanceinc.com" target="_blank">Jack McKernan</a></li><li>Template: <a href="http://html5up.net" target="_blank">HTML5 UP</a></li>
+                            </ul>
+                        </div>
+                    </section>
 
-			</div>
+            </div>
 
-		<!-- Scripts -->
-			<script src="assets/js/skel.min.js"></script>
-			<script src="assets/js/jquery.min.js"></script>
-			<script src="assets/js/jquery.scrollex.min.js"></script>
-			<script src="assets/js/util.js"></script>
-			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
-			<script src="assets/js/main.js"></script>
+        <!-- Scripts -->
+            <script src="assets/js/skel.min.js"></script>
+            <script src="assets/js/jquery.min.js"></script>
+            <script src="assets/js/jquery.scrollex.min.js"></script>
+            <script src="assets/js/util.js"></script>
+            <!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+            <script src="assets/js/main.js"></script>
             <script>
                 (function (i, s, o, g, r, a, m) {
                     i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
@@ -209,10 +209,10 @@
                 }
             </script>
 
-	</body>
+    </body>
 </html>
 <!--
-	Solid State by HTML5 UP
-	html5up.net | @n33co
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+    Solid State by HTML5 UP
+    html5up.net | @n33co
+    Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 -->
--- restricted/schedule-upload.php
+++ restricted/schedule-upload.php
@@ -14,16 +14,16 @@ if (isset($_POST['submit']))
 {
     $month = $_POST['month'];
     $overwritecurrent = $_POST['overwritecurrent'];
-	$filename = $_FILES["file"]["name"];
-	$file_basename = substr($filename, 0, strripos($filename, '.')); // get file name
-	$file_ext = substr($filename, strripos($filename, '.')); // get file extension
-	$filesize = $_FILES["file"]["size"];
-	$allowed_file_types = array('.pdf');	
+    $filename = $_FILES["file"]["name"];
+    $file_basename = substr($filename, 0, strripos($filename, '.')); // get file name
+    $file_ext = substr($filename, strripos($filename, '.')); // get file extension
+    $filesize = $_FILES["file"]["size"];
+    $allowed_file_types = array('.pdf');    
 
-	if (in_array($file_ext, $allowed_file_types) && ($filesize < 200000))
-	{	
-		// Rename file
-		$newfilename = "current-schedule-yoga-balance" . $file_ext;
+    if (in_array($file_ext, $allowed_file_types) && ($filesize < 200000))
+    {    
+        // Rename file
+        $newfilename = "current-schedule-yoga-balance" . $file_ext;
         $backupfilename = strtolower(date('F')) . "-" . date('Y') . "-schedule-yoga-balance" . $file_ext;
         if(TRUE) //file_exists("../downloads/schedule/" . $backupfilename
         {
@@ -58,27 +58,27 @@ if (isset($_POST['submit']))
         }
         
         echo "<iframe src=\"../downloads/schedule/\" id=\"fileviewer\" width=\"90%\" height=\"600px\"></iframe>";
-        	
-	}
-	elseif (empty($file_basename))
-	{	
-		// file selection error
-		echo "Please select a file to upload.";
+            
+    }
+    elseif (empty($file_basename))
+    {    
+        // file selection error
+        echo "Please select a file to upload.";
         echo "<br><br><a href=\"javascript:history.back()\">Back</a>";
-	} 
-	elseif ($filesize > 200000)
-	{	
-		// file size error
-		echo "The file you are trying to upload is too large.";
+    } 
+    elseif ($filesize > 200000)
+    {    
+        // file size error
+        echo "The file you are trying to upload is too large.";
         echo "<br><br><a href=\"javascript:history.back()\">Back</a>";
-	}
-	else
-	{
-		// file type error
-		echo "Only these file types are allowed for upload: " . implode(', ',$allowed_file_types);
-		unlink($_FILES["file"]["tmp_name"]);
+    }
+    else
+    {
+        // file type error
+        echo "Only these file types are allowed for upload: " . implode(', ',$allowed_file_types);
+        unlink($_FILES["file"]["tmp_name"]);
         echo "<br><br><a href=\"javascript:history.back()\">Back</a>";
-	}
+    }
 }
 
 ?>
